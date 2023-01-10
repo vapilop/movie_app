2c6432309758d95f48d11c2b3838a10c415cd9c6
vapilop <vtyurina.polina@gmail.com>
2023-01-11 00:24:02 +0300

    Updates

---
+++ precache-manifest.60e6080b7949509c61a6043b3148b215.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c88f240f0d23855cde02",
+    "url": "/movie_app/static/css/main.4205a705.chunk.css"
+  },
+  {
+    "revision": "c88f240f0d23855cde02",
+    "url": "/movie_app/static/js/main.749a9c7b.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "6cf2e0ddfd20212df95e",
+    "url": "/movie_app/static/js/2.4186579b.chunk.js"
+  },
+  {
+    "revision": "8969a89b3f8c054d86526f00c0e2458b",
+    "url": "/movie_app/index.html"
+  }
+];
